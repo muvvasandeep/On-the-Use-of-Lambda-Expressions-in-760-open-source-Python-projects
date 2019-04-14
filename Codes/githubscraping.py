58d38d301ecdebef1f19208959073d28982f5739
muvvasandeep_cs16b017 <35159794+muvvasandeep@users.noreply.github.com>
2019-04-14 13:48:10 +0530

    Updated github scraping file
    Added code for storing repo and number of stars in a json file(result.json)

--- Codes/githubscraping.py
+++ Codes/githubscraping.py
@@ -16,6 +16,7 @@ repo = []
 temp = []
 
 repo_stars=dd(str)
+
 def repo_name(link):
     x=''
     for i in range(len(link)-1,-1,-1):
@@ -42,34 +43,36 @@ def login(driver,usr,pwd):
 def extract_repo(driver):
     reposit=driver.find_elements_by_class_name('v-align-middle')
     stars=driver.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div/ul/li/div[2]/div[2]')
-
+    temp_repo = []
     for i in range(0,len(reposit)):
         link=reposit[i].get_attribute('href')
         if link!=None:
             repo.append(reposit[i].get_attribute('href'))
-    for i in range(0,len(repo)):
-        name=repo_name(repo[i])
+            temp_repo.append(reposit[i].get_attribute('href'))
+    print(len(temp_repo),len(stars))
+    for i in range(0,len(temp_repo)):
+        name=repo_name(temp_repo[i])
         repo_stars[name+'-master']=stars[i].text
         
     lang = driver.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div/ul/li/div[2]/div[1]')
     stars=driver.find_elements_by_xpath('//*[@id="js-pjax-container"]/div/div[3]/div/ul/li/div[2]/div[2]')
-    print(len(lang))
-    print(len(stars))
+    #print(len(lang))
+    #print(len(stars))
     for i in range(0,len(lang)):
         pyt = lang[i].text
-        star=stars[i].text
+        #star=stars[i].text
         if pyt!=None:
             temp.append(pyt)
         
 options = Options()
 options.add_argument("--disable-notifications") 
-driver = webdriver.Chrome(r"C:\Users\Shubham\Downloads\chromedriver_win32\chromedriver.exe",chrome_options=options)
+driver = webdriver.Chrome(r"C:\Users\muvva\Documents\chromedriver.exe",chrome_options=options)
 usr="shubham.rsangle@gmail.com"
 pwd ="cs16b026"
 login(driver,usr,pwd)
 url='https://github.com/'
 search="python"
-num_pages=1
+num_pages=100
 sear=search.split(' ')
 search_url=''
 for j in sear:
@@ -79,8 +82,11 @@ print(search_url)
 
 for z in range(1,1+num_pages):
     driver.get(url+'search?o=desc&p='+str(z)+'&q='+search_url+'&s=stars&type=Repositories')
+    sleep(3)
     print("Item searched")
     extract_repo(driver)
+
+
 for i in range(0,len(repo)):
     dic[temp[i]].append(repo[i])
 python_repos = dic["Python"]
@@ -92,6 +98,7 @@ for i in range(0,len(python_repos)):
         sleep(5)
     except:
         continue
+
 import json
 with open('result.json', 'w') as fp:
     json.dump(repo_stars, fp)
