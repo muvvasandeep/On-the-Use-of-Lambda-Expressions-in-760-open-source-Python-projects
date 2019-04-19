b8aeb05d906d3271293b8ba2dc669c85d9908624
shubhamrsangle <36818228+shubhamrsangle@users.noreply.github.com>
2019-04-19 17:02:57 +0530

    Add files via upload

--- Codes/githubscraping.py
+++ Codes/githubscraping.py
@@ -12,6 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from collections import defaultdict as dd
 dic=dd(list)
 url = "https://github.com/login"
+fsandeep=open("failure.txt",'a',encoding='utf-8')
 repo = []
 temp = []
 
@@ -66,13 +67,13 @@ def extract_repo(driver):
         
 options = Options()
 options.add_argument("--disable-notifications") 
-driver = webdriver.Chrome(r"C:\Users\muvva\Documents\chromedriver.exe",chrome_options=options)
+driver = webdriver.Chrome(r"C:\Users\Shubham\Downloads\chromedriver_win32\chromedriver.exe",chrome_options=options)
 usr="shubham.rsangle@gmail.com"
 pwd ="cs16b026"
 login(driver,usr,pwd)
 url='https://github.com/'
 search="python"
-num_pages=100
+num_pages=1
 sear=search.split(' ')
 search_url=''
 for j in sear:
@@ -81,7 +82,8 @@ search_url=search_url[:-1]
 print(search_url)
 
 for z in range(1,1+num_pages):
-    driver.get(url+'search?o=desc&p='+str(z)+'&q='+search_url+'&s=stars&type=Repositories')
+    driver.get(url+'search?l=Python&o=desc&p='+str(z)+'&q=Python&s=stars&type=Repositories')
+    #https://github.com/search?l=Python&o=desc&p=66&q=Python&s=stars&type=Repositories
     sleep(3)
     print("Item searched")
     extract_repo(driver)
@@ -91,14 +93,22 @@ for i in range(0,len(repo)):
     dic[temp[i]].append(repo[i])
 python_repos = dic["Python"]
 
+#driver.get('https://github.com/ansible/ansible/archive/master.zip')
+
 for i in range(0,len(python_repos)):
     try:
         print(python_repos[i])
         driver.get(python_repos[i]+'/archive/master.zip')
+        if "404: Not Found" in driver.page_source:
+            fsandeep.write(python_repos[i])
+            driver.get(python_repos[i])
         sleep(5)
+
     except:
+        
         continue
 
+fsandeep.close()
 import json
 with open('result.json', 'w') as fp:
     json.dump(repo_stars, fp)
